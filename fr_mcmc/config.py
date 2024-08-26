a86fe5290e2e5acc0d72275e1f29af8a2a80850c
matiasleize <matias.leize@hotmail.com>
2024-08-26 09:00:08 -0300

    Miscellaneous

--- fr_mcmc/config.py
+++ fr_mcmc/config.py
@@ -13,8 +13,8 @@ os.chdir(path_git + '/configs/')
 
 #Here you have to specify the the name of your .yml file
 
-#yml_file = 'config_GILA_4p_PPS_CC_0.75.yml'
-yml_file = 'config_GILA_4p_PPS_CC_0.80.yml'
+yml_file = 'config_GILA_4p_PPS_CC_0.75.yml'
+#yml_file = 'config_GILA_4p_PPS_CC_0.80.yml'
 #yml_file = 'config_GILA_4p_PPS_CC_0.85.yml'
 #yml_file = 'config_GILA_4p_PPS_CC_0.90.yml'
 #yml_file = 'config_GILA_4p_PPS_CC_1.00.yml'
