45bbe8eb0c83d611f1044772135b4575f988a763
matiasleize <matias.leize@hotmail.com>
2024-09-30 10:01:34 -0300

    Miscellaneous

--- fr_mcmc/utils/LambdaCDM.py
+++ fr_mcmc/utils/LambdaCDM.py
@@ -12,35 +12,35 @@ from scipy.constants import c as c_luz #meters/seconds
 c_luz_km = c_luz/1000
 #%%
 
-def E_LCDM(z, omega_m):
+def E_LCDM(z, Omega_m):
     '''
     Calculation of the normalized Hubble parameter, independent
     of the Hubble constant H0.
     '''
-    omega_lambda = 1 - omega_m
-    E = np.sqrt(omega_m * (1 + z)**3 + omega_lambda)
+    Omega_lambda = 1 - Omega_m
+    E = np.sqrt(Omega_m * (1 + z)**3 + Omega_lambda)
     return E
 
-def H_LCDM(z, omega_m, H_0):
+def H_LCDM(z, Omega_m, H_0):
     '''
     Calculation of the Hubble parameter.
     Here we neclect the radiation (it holds 
     that \Omega_r + \Omega_m + \Omega_L = 1).
     '''
-    H = H_0 * E_LCDM(z, omega_m)
+    H = H_0 * E_LCDM(z, Omega_m)
     return H
 
-def H_LCDM_rad(z, omega_m, H_0):
+def H_LCDM_rad(z, Omega_m, H_0):
     '''
     Calculation of the Hubble parameter. Here it holds that
     \Omega_r + \Omega_m + \Omega_L = 1 
     '''
-    omega_r = 4.18343*10**(-5) / (H_0/100)**2
-    omega_lambda = 1 - omega_m - omega_r
+    Omega_r = 4.18343*10**(-5) / (H_0/100)**2
+    Omega_lambda = 1 - Omega_m - Omega_r
 
     if isinstance(z, (np.ndarray, list)):
-        H = H_0 * np.sqrt(omega_r * (1 + z)**4 + omega_m * (1 + z)**3 + omega_lambda)
+        H = H_0 * np.sqrt(Omega_r * (1 + z)**4 + Omega_m * (1 + z)**3 + Omega_lambda)
     else:
-        H = H_0 * (omega_r * (1 + z)**4 + omega_m * (1 + z)**3 + omega_lambda)**(1/2)
+        H = H_0 * (Omega_r * (1 + z)**4 + Omega_m * (1 + z)**3 + Omega_lambda)**(1/2)
 
     return H
