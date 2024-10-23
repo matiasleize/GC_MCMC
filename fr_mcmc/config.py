32a416dd766f169bbf84a381a1c11774e5abd62a
matiasleize <matias.leize@hotmail.com>
2024-10-23 02:46:29 -0300

    Update configs

--- fr_mcmc/config.py
+++ fr_mcmc/config.py
@@ -13,30 +13,22 @@ os.chdir(path_git + '/configs/')
 
 #Here you have to specify the the name of your .yml file
 
-#yml_file = 'config.yml'
-
-#yml_file = 'config_GILA_4p_PPS_CC_0.75.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.80.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.85.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.90.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_1.00.yml'
-
-yml_file = 'config_GILA_4p_PPS_CC_0.90_beta_long.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.90_beta_short.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.90_gila_long.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_0.90_gila_short.yml'
-
-#yml_file = 'config_GILA_5p_PPS_CC_beta_long.yml'
-#yml_file = 'config_GILA_5p_PPS_CC_gila_long.yml'
-
 #yml_file = 'config_LCDM_3p_PPS_CC.yml'
+#yml_file = 'config_LCDM_4p_PPS_CC_DESI.yml'
 
-#yml_file = 'config_GILA_4p_PPS_CC_BAO_0.75.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_BAO_0.80.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_BAO_0.85.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_BAO_0.90.yml'
-#yml_file = 'config_GILA_4p_PPS_CC_BAO_1.00.yml'
+#yml_file = 'config_BETA_3p_PPS_CC_0.85.yml'
+#yml_file = 'config_BETA_3p_PPS_CC_0.90.yml'
+#yml_file = 'config_BETA_3p_PPS_CC_0.95.yml'
+#yml_file = 'config_BETA_4p_PPS_CC_DESI_0.85.yml'
+#yml_file = 'config_BETA_4p_PPS_CC_DESI_0.90.yml'
+#yml_file = 'config_BETA_4p_PPS_CC_DESI_0.95.yml'
 
+#yml_file = 'config_GILA_3p_PPS_CC_0.85.yml'
+#yml_file = 'config_GILA_3p_PPS_CC_0.90.yml'
+#yml_file = 'config_GILA_3p_PPS_CC_0.95.yml'
+#yml_file = 'config_GILA_4p_PPS_CC_DESI_0.85.yml'
+yml_file = 'config_GILA_4p_PPS_CC_DESI_0.90.yml'
+#yml_file = 'config_GILA_4p_PPS_CC_DESI_0.95.yml'
 
 with open(yml_file, "r") as ymlfile:
     full_cfg = yaml.safe_load(ymlfile)
